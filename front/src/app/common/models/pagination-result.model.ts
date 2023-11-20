306187da39a4d527d68728c79bada294547944ab
hmztplinv <hmz.tpal@gmail.com>
2023-11-20 14:59:51 +0300

    product list html

--- front/src/app/common/models/pagination-result.model.ts
+++ front/src/app/common/models/pagination-result.model.ts
@@ -1,8 +1,12 @@
 export class PaginationResultModel<T> {
-    datas:T[]=[];
+    datas: T;
     pageNumber = 1;
     pageSize = 10;
     isFirstPage = true;
     isLastPage = false;
     totalPageCount = 0;
+
+    // constructor(datas: T) {
+    //     this.datas = datas;
+    // }
 }
