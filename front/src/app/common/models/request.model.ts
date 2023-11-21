fd44b3291887229f48f687e1b0e652d4ccfb0dff
hmztplinv <hmz.tpal@gmail.com>
2023-11-21 17:05:38 +0300

    home page get product

--- front/src/app/common/models/request.model.ts
+++ front/src/app/common/models/request.model.ts
@@ -2,4 +2,7 @@ export class RequestModel{
     pageNumber:number=1;
     pageSize:number=10;
     search:string="";
+    categoryName:string="All";
+    priceFilter:string="0";
+    categoryId:string="";
 }
--- front/src/app/components/home/home.component.ts
+++ front/src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from 'src/app/common/shared/shared.module';
+import { CategoryModel } from '../categories/models/category.model';
+import { CategoryService } from '../categories/services/category.service';
+import { RequestModel } from 'src/app/common/models/request.model';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,26 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  categories: CategoryModel[] = [];
+  requestModel : RequestModel=new RequestModel();
+
+  constructor(private categoryService:CategoryService) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.getAll(res => this.categories = res);
+  }
+
+  changeCategory(categoryId:string,categoryName:string) {
+    this.requestModel.categoryName = categoryName;
+    this.requestModel.categoryId = categoryId;
+  }
+
+
 
 }
